488030cd911ea5a0cf56d2f39fa51b9bbf489bde
JosephCass <josephcassinera@gmail.com>
2022-11-22 13:15:06 -0500

    Added the navbar heading and navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,5 +2,32 @@ import { Link } from "react-router-dom";
 import cartIcon from "../assets/cart-shopping-solid.svg";
 
 export default function Navbar() {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="heading-container">
+        <h2 className="site-heading">Shoe Store</h2>
+      </div>
+      <div className="links-container">
+        <Link className="navbar-link" to="/">
+          Home
+        </Link>
+        <Link className="navbar-link" to="/products">
+          Products
+        </Link>
+        <Link className="navbar-link" to="/contact">
+          Contact
+        </Link>
+        <div className="shopping-cart-container">
+          <img
+            width="25px"
+            height="25px"
+            src={cartIcon}
+            className="shopping-cart-icon"
+            alt=""
+          />
+          <span className="shopping-cart-amount"></span>
+        </div>
+      </div>
+    </div>
+  );
 }
