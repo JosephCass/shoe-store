ea5b0300f6b69e7a0a4ce1cadaffa5be303897ce
JosephCass <josephcassinera@gmail.com>
2022-11-23 09:54:28 -0500

    Finished adding features to home page component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+import shoePoster from "../assets/shoeposter.jpg";
+
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="poster-container">
+        <img
+          className="home-poster"
+          src={shoePoster}
+          alt="Shoe ad poster"
+        ></img>
+        <Link className="home-shop-btn" to="/products">
+          Shop Now
+        </Link>
+      </div>
+    </div>
+  );
 }
